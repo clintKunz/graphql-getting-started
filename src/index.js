806a67ced8eb4157073ed2d2b74309463c9b30c3
Clint <clintkunz@gmail.com>
2019-03-21 15:16:55 -0600

    server connected to hosted db

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
-
-  let idCount = links.length; 
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => links.find(link => link.id === args.id)
+        feed: (root, args, ctx, info) => {
+            return ctx.prisma.links()
+        }
+        //link: (parent, args) => links.find(link => link.id === args.id)
     },
 
     Mutation: {
-        post: (parents, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
+        post: (root, args, ctx) => {
+            return ctx.prisma.createLink({
                 url: args.url,
-            }
-            links.push(link)
-            return link
+                description: args.description
+            })
         },
-        updateLink: (parents, args) => {
-            const link = links.find(link => link.id === `link-${args.id}`)
-            link.url = args.url;
-            link.description = args.description;
-            return link;
-        },
-        deleteLink: (parents, args) => {
-            links = links.filter(link => link.id !== `link-${args.id}`)
-            return `You deleted link-${args.id}`;
-        }
+        // updateLink: (parents, args) => {
+        //     const link = links.find(link => link.id === `link-${args.id}`)
+        //     link.url = args.url;
+        //     link.description = args.description;
+        //     return link;
+        // },
+        // deleteLink: (parents, args) => {
+        //     links = links.filter(link => link.id !== `link-${args.id}`)
+        //     return `You deleted link-${args.id}`;
+        // }
     }
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: { prisma }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
